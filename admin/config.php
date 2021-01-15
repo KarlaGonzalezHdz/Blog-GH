53281888d8cf0bc55709480db9f2cc633500c2ad
Karla GH <karlahdz.98@outlook.com>
2021-01-15 12:06:34 -0600

    estructura del proyecto y primeros archivos de estilos

---
+++ admin/config.php
@@ -0,0 +1,22 @@
+<?php
+
+    define('RUTA', 'http://localhost/PHP-MYSQL/practicas/Blog/');
+
+    $bd_config = array(
+        'basedatos' => 'blog_practica',
+        'usuario' => 'root',
+        'pass' => ''
+    );
+
+
+    $blog_config = array(
+        'post_por_pagina' => '2',
+        'carpeta_imagenes' => 'imagenes/'
+    );
+
+    $blog_admin = array(
+        'usuario' => 'Karla', 
+        'password' => '123'
+    );
+
+?>
